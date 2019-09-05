e3ebcfabc6d20e179b8c5f992a6d6d50fd29c5f7
galoz <gal.oz@powtoon.com>
2019-09-05 15:34:42 +0300

    first version

--- lib.py
+++ lib.py
@@ -1,18 +1,6 @@
 from PIL import Image
 
 
-def grey_highpass(limit: int, pixel_value: int, hard=False) -> int:
-    if hard:
-        val = 255 if pixel_value >= limit else 0
-    else:
-        val = pixel_value if pixel_value >= limit else 0
-    return val
-
-
-def binary_highpass(pixel_value, limit):
-    return pixel_value >= limit
-
-
 def resize_by_multiples(image, multiple=9, max_val=1720):
     h = image.height
     w = image.width
@@ -30,17 +18,3 @@ def resize_by_multiples(image, multiple=9, max_val=1720):
         return image.resize((w_diff, h_diff), Image.NEAREST)
     else:
         return image
-
-
-class GreyCharacter:
-
-    def __init__(self, char_representation, bool_kernel):
-        self.char_representation = char_representation
-        self.bool_kernel = bool_kernel
-
-    def similarity_score(self, ext_bool_kernel):
-        assert(len(ext_bool_kernel) == len(self.bool_kernel)), "kernel sizes are not the same!"
-        score = 0
-        for pix1, pix2 in zip(self.bool_kernel, ext_bool_kernel):
-            if pix1 == pix2:
-                score += 1
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-from PIL import Image, ImageFilter, ImageOps
-from lib import grey_highpass, resize_by_multiples, GreyCharacter
-from collections import Counter
+import argparse
 
+from PIL import Image, ImageFilter, ImageOps
+from lib import resize_by_multiples
+from collections import Counter
 from colr import Colr as c
 
-KERN = 3
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help="path to image file")
+parser.add_argument('--kernel', dest='kernel', metavar='K', type=int, default=3, help="size of the kernel")
+parser.add_argument('--no-color', help="Print without coloration", action='store_true')
+args = vars(parser.parse_args())
+
+KERN = args['kernel']
+
+assert(args.get('path', 0)), "A file path was not provided!"
 
 ascii_fillers = ["*", "&", "#", "@", "0", "%", "^", "S", "=", "~", ":" ","]
 
-
-color_img = resize_by_multiples(Image.open("ravi.jpeg"), KERN)
+orig = resize_by_multiples(Image.open(args['path']), KERN)
 mode_filter = ImageFilter.ModeFilter(size=KERN)
 
-color_img = color_img.filter(mode_filter).filter(ImageFilter.EDGE_ENHANCE_MORE)
+color_img = orig.filter(mode_filter).filter(ImageFilter.EDGE_ENHANCE_MORE)
 color_img = ImageOps.posterize(color_img, 3)
 grey_img = color_img.convert("L")
-# clamp_img = grey_img.point(lambda i: grey_highpass(80, i, hard=True))
 
-# bin_pxl = clamp_img.load()
+orig = ImageOps.autocontrast(orig.filter(ImageFilter.EDGE_ENHANCE_MORE), 10).filter(ImageFilter.GaussianBlur)
 grey_pxl = grey_img.load()
-color_pxl = color_img.load()
+color_pxl = orig.load()
 
 
 min_grey_value = 254//(len(ascii_fillers)-1)
@@ -28,18 +35,13 @@ w, h = grey_img.size
 for y_step in range(0, h, KERN*KERN):
     line = []  # [(char, color), ...]
     for x_step in range(0, w, KERN*KERN):
-        # binary_kern = [bin_pxl[x_step+x, y_step+y] for y in range(KERN) for x in range(KERN)])
         grey_kern = Counter([grey_pxl[x_step+x, y_step+y] for y in range(KERN) for x in range(KERN)]).most_common()
         color_kern = Counter([color_pxl[x_step+x, y_step+y] for y in range(KERN) for x in range(KERN)]).most_common()
         char = ascii_fillers[grey_kern[0][0]//min_grey_value]
         color = color_kern[0][0]
         line.append((char, color))
 
-    print(*[c().rgb(*color, char) for char, color in line])
-    # print([(*color, char) for char, color in line])
-
-
-
-#
-# grey_img.save('ravipost.jpeg')
-# color_img2.save('posterravi.jpeg')
+    if args.get('no-color', 0):
+        print(*[char for char, color in line])
+    else:
+        print(*[c().rgb(*color, char) for char, color in line])
